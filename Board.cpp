4ff55c56fb43bc90a35e5cf1dc67562e41571b7b
AdiDahari <dahariadi16@gmail.com>
2021-03-16 12:48:31 +0200

    progress

--- Board.cpp
+++ Board.cpp
@@ -1,16 +1,52 @@
 #include "Board.hpp"
 #include "Direction.hpp"
 #include <string>
+#include <map>
+#include <math.h>
 #include <iostream>
 using namespace std;
-
+const int INF = numeric_limits<int>::max();
 namespace ariel
 {
     class Board
     {
     private:
         string b;
-        int MAX_ROW = 0, MAX_COL = 0;
+        int MAX_ROW = 0, MAX_COL = 0, MIN_ROW = INF, MIN_COL = INF;
+        map<int, map<int, char>> _board;
+
+        void insert_horizontal(int row, int col, string msg)
+        {
+            if (row < MIN_ROW)
+                MIN_ROW = row;
+            else if (row > MAX_ROW)
+                MAX_ROW = row;
+            if (col + msg.length() > MAX_COL)
+                MAX_COL = col + msg.length();
+            else if (col < MIN_COL)
+                MIN_COL = col;
+
+            for (int i = 0; i < msg.length(); i++)
+            {
+                _board[row][col + i] = msg.at(i);
+            }
+        }
+        void insert_vertical(int row, int col, string msg)
+        {
+            if (row < MIN_ROW)
+                MIN_ROW = row;
+            else if (row > MAX_ROW)
+                MAX_ROW = MAX_ROW + msg.length();
+            if (col + msg.length() > MAX_COL)
+                MAX_COL = col;
+            else if (col < MIN_COL)
+                MIN_COL = col;
+
+            for (int i = 0; i < msg.length(); i++)
+            {
+                _board[row + i][col] = msg.at(i);
+            }
+        }
 
     public:
         void post(int row, int column, Direction d, string msg)
@@ -18,6 +54,18 @@ namespace ariel
             if (row < 0 || column < 0 || msg == "" || (d != Direction::Horizontal && d != Direction::Vertical))
             {
                 __throw_invalid_argument("One or more of the input arguments are not valid");
+            }
+
+            else
+            {
+                if (d == Direction::Horizontal)
+                {
+                    insert_horizontal(row, column, msg);
+                }
+                else
+                {
+                    insert_vertical(row, column, msg);
+                }
             }
         }
         string read(int row, int column, Direction d, int length)
@@ -27,7 +75,7 @@ namespace ariel
                 __throw_invalid_argument("One or more of the input arguments are not valid");
             }
 
-            if (row > MAX_ROW || column > MAX_COL)
+            if (row > MAX_ROW || column > MAX_COL || (d == Direction::Horizontal && MAX_COL < column + length) || (d == Direction::Vertical && MAX_ROW < row + length))
             {
                 __throw_invalid_argument("Desired coordinates does not exists");
             }
