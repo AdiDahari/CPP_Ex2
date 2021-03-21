dbd9f80c2c96aa000f60b7d38e5333f20837a7b5
AdiDahari <dahariadi16@gmail.com>
2021-03-21 10:23:49 +0200

    new implementation

--- Board.cpp
+++ Board.cpp
@@ -1,87 +1,152 @@
+#include <iostream>
+#include <string>
+#include <vector>
 #include "Board.hpp"
 #include "Direction.hpp"
-#include <string>
-#include <map>
-#include <math.h>
-#include <iostream>
 using namespace std;
-const int INF = numeric_limits<int>::max();
+
 namespace ariel
 {
-    class Board
+    void Board::post(unsigned int r, unsigned int c, Direction d, string msg)
     {
-    private:
-        string b;
-        int MAX_ROW = 0, MAX_COL = 0, MIN_ROW = INF, MIN_COL = INF;
-        map<int, map<int, char>> _board;
-
-        void insert_horizontal(int row, int col, string msg)
+        if (msg.length() < 1 || r < 0 || c < 0)
         {
-            if (row < MIN_ROW)
-                MIN_ROW = row;
-            else if (row > MAX_ROW)
-                MAX_ROW = row;
-            if (col + msg.length() > MAX_COL)
-                MAX_COL = col + msg.length();
-            else if (col < MIN_COL)
-                MIN_COL = col;
-
-            for (int i = 0; i < msg.length(); i++)
-            {
-                _board[row][col + i] = msg.at(i);
-            }
+            __throw_invalid_argument("Invalid Input");
         }
-        void insert_vertical(int row, int col, string msg)
+        else
         {
-            if (row < MIN_ROW)
-                MIN_ROW = row;
-            else if (row > MAX_ROW)
-                MAX_ROW = MAX_ROW + msg.length();
-            if (col + msg.length() > MAX_COL)
-                MAX_COL = col;
-            else if (col < MIN_COL)
-                MIN_COL = col;
 
-            for (int i = 0; i < msg.length(); i++)
-            {
-                _board[row + i][col] = msg.at(i);
-            }
+            bool flag = (d == Direction::Horizontal);
+            flag ? post_horizontal(r, c, msg) : post_vertical(r, c, msg);
         }
+    }
 
-    public:
-        void post(int row, int column, Direction d, string msg)
+    string Board::read(unsigned int r, unsigned int c, Direction d, unsigned int length)
+    {
+        // cout << "ARGS: r = " + to_string(r) + " ,r + length = " + to_string(r + length) + " ,c = " + to_string(c) + " ,c + length= " + to_string(c + length) + " , MAX_ROW = " + to_string(MAX_ROW) + " , MIN_ROW = " + to_string(MIN_ROW) + " , MAX_COL = " + to_string(MAX_COL) + " , MIN_COL = " + to_string(MIN_COL) << endl;
+        string msg;
+        if ((d == Direction::Horizontal && (r < MIN_ROW || r > MAX_ROW || c < MIN_COL || c + length - 1 > MAX_COL)) ||
+            (d == Direction::Vertical && (r < MIN_ROW || r + length - 1 > MAX_ROW || c < MIN_COL || c > MAX_COL)))
         {
-            if (row < 0 || column < 0 || msg == "" || (d != Direction::Horizontal && d != Direction::Vertical))
+            __throw_out_of_range("Out of Board's range");
+        }
+        else
+        {
+            bool flag = (d == Direction::Horizontal);
+            if (flag)
             {
-                __throw_invalid_argument("One or more of the input arguments are not valid");
+                msg = read_horizontal(r, c, length);
             }
-
             else
             {
-                if (d == Direction::Horizontal)
+                msg = read_vertical(r, c, length);
+            }
+            return msg;
+        }
+    }
+    void Board::show()
+    {
+        for (unsigned int i = 0; i < b.size(); i++)
+        {
+            cout << to_string(i + MIN_ROW) + ":\t";
+            for (unsigned int j = 0; j < b[i].size(); j++)
+            {
+                cout << b[i][j];
+            }
+            cout << endl;
+        }
+    }
+
+    void Board::post_horizontal(unsigned int r, unsigned int c, string msg)
+    {
+        if (r - 1 < MIN_ROW)
+        {
+            MIN_ROW = r - 1;
+        }
+
+        if (r + 1 > MAX_ROW)
+        {
+            MAX_ROW = r + 2;
+        }
+        if (c - 2 < MIN_COL)
+        {
+            MIN_COL = c - 2;
+        }
+        if (c + 2 + msg.length() > MAX_COL)
+        {
+            MAX_COL = c + 2 + msg.length();
+        }
+        for (unsigned int i = 0; i < msg.length(); i++)
+        {
+            letters[r][c + i] = msg.at(i);
+        }
+
+        rebuild_board();
+    }
+
+    void Board::post_vertical(unsigned int r, unsigned int c, string msg)
+    {
+        if (r - 1 < MIN_ROW)
+        {
+            MIN_ROW = r - 1;
+        }
+
+        if (r + msg.length() + 1 > MAX_ROW)
+        {
+            MAX_ROW = r + 1 + msg.length();
+        }
+        if (c - 2 < MIN_COL)
+        {
+            MIN_ROW = c - 2;
+        }
+        if (c + 2 > MAX_COL)
+        {
+            MAX_COL = c + 2;
+        }
+        for (unsigned int i = 0; i < msg.length(); i++)
+        {
+            letters[r + i][c] = msg.at(i);
+        }
+        rebuild_board();
+    }
+
+    string Board::read_horizontal(unsigned int r, unsigned int c, unsigned int length)
+    {
+        string msg;
+        for (unsigned int i = 0; i < c + length - MIN_COL; i++)
+        {
+            msg.push_back(b[r - MIN_ROW][c - MIN_COL + i]);
+        }
+        return msg;
+    }
+
+    string Board::read_vertical(unsigned int r, unsigned int c, unsigned int length)
+    {
+        string msg;
+        for (unsigned int i = 0; i < r + length - MIN_ROW; i++)
+        {
+            msg.push_back(b[r - MIN_ROW + i][c - MIN_COL]);
+        }
+
+        return msg;
+    }
+
+    void Board::rebuild_board()
+    {
+        b = vector<vector<char>>((MAX_ROW - MIN_ROW), vector<char>(MAX_COL - MIN_COL, '_'));
+        for (auto const &[k1, v1] : letters)
+        {
+            for (auto const &[k2, v2] : v1)
+            {
+                if (v2 == '\t' || v2 == ' ')
                 {
-                    insert_horizontal(row, column, msg);
+                    b[k1 - MIN_ROW][k2 - MIN_COL];
                 }
                 else
                 {
-                    insert_vertical(row, column, msg);
+                    b[k1 - MIN_ROW][k2 - MIN_COL] = v2;
                 }
             }
         }
-        string read(int row, int column, Direction d, int length)
-        {
-            if (row < 0 || column < 0 || length < 1 || (d != Direction::Horizontal && d != Direction::Vertical))
-            {
-                __throw_invalid_argument("One or more of the input arguments are not valid");
-            }
-
-            if (row > MAX_ROW || column > MAX_COL || (d == Direction::Horizontal && MAX_COL < column + length) || (d == Direction::Vertical && MAX_ROW < row + length))
-            {
-                __throw_invalid_argument("Desired coordinates does not exists");
-            }
-        }
-        void show()
-        {
-        }
-    };
+    }
 }
--- Board.hpp
+++ Board.hpp
@@ -1,12 +1,39 @@
 #pragma once
+#include "Direction.hpp"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <limits>
+using namespace std;
 
+const unsigned int INF = numeric_limits<unsigned int>::max();
 namespace ariel
 {
     class Board
     {
+    private:
+        vector<vector<char>> b;
+        map<unsigned int, map<unsigned int, char>> letters;
+        unsigned int MIN_ROW, MIN_COL, MAX_ROW, MAX_COL;
+        void post_horizontal(unsigned int r, unsigned int c, string msg);
+        void post_vertical(unsigned int r, unsigned int c, string msg);
+        string read_horizontal(unsigned int r, unsigned int c, unsigned int length);
+        string read_vertical(unsigned int r, unsigned int c, unsigned int length);
+        void rebuild_board();
+
     public:
-        void post(int row, int column, Direction d, string msg);
-        string read(int row, int column, Direction d, int length);
+        Board()
+        {
+            b = vector<vector<char>>(3, vector<char>(3));
+            letters = map<unsigned int, map<unsigned int, char>>();
+            MIN_ROW = MIN_COL = INF;
+            MAX_ROW = MAX_COL = 0;
+        }
+        ~Board(){};
+
+        void post(unsigned int r, unsigned int c, Direction d, string msg);
+        string read(unsigned int r, unsigned int c, Direction d, unsigned int length);
         void show();
     };
 }
